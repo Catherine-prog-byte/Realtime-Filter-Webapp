025749adb112f208eb65a7408e63697930d176ba
Catherine-prog-byte <77283675+Catherine-prog-byte@users.noreply.github.com>
2021-09-08 19:40:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+noseX="";
+noseY="";
+
+function preload() {
+}
+function setup() {
+    canvas = createCanvas(450,450)
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(450,450);
+    video.hide();
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+function draw() {
+    image(video,0,0,450,450);
+    fill('#ff0000');
+    stroke('#ff0000');
+    circle(noseX,noseY,20);
+}
+function take_snapshot() {
+   save('myFilterImage.png')
+}
+function modelLoaded() {
+    console.log("poseNet is initialized")
+}
+function gotPoses(results) {
+    if (results.length >0) {
+        console.log(results);
+        noseX=results[0].pose.nose.x;
+        noseY=results[0].pose.nose.y;
+        console.log("noseX="+noseX);
+        console.log("noseY="+noseY);
+    }
+}
